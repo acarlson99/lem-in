e68c42502bfcba84ff9db3f9dbb2ad322ad1b096
Clayton Allen <callen@student.42.us.org>
2019-01-03 18:27:06 -0800

    IT BE FIXED DAWGGZ! (rewrote lstpush and fixed read_connection)

--- srcs/main.c
+++ srcs/main.c
@@ -135,19 +135,15 @@ void		read_connections(t_lem *info, char **line)
 	tmp[0] = info->rooms;
 	while (tmp[0])
 	{
-		ft_printf("rc.tmp0.lstlen(info->rooms) = %u\n", lstlen(info->rooms));
-		if (!ft_strcmp(tmp[0]->r->name, line[0]))
+		if (tmp[0]->r && tmp[0]->r->name && !ft_strcmp(tmp[0]->r->name, line[0]))
 		{
 			tmp[1] = info->rooms;
 			while (tmp[1])
 			{
-				ft_printf("rc.tmp1.lstlen(info->rooms) = %u\n", lstlen(info->rooms));
 				if (!ft_strcmp(tmp[1]->r->name, line[1]))
 				{
-					ft_printf("rc.lstpush.lstlen(info->rooms) = %u\n", lstlen(info->rooms));
 					lstpush(&tmp[0]->r->connections, tmp[1]->r);
 					lstpush(&tmp[1]->r->connections, tmp[0]->r);
-					ft_printf("rc.break.lstlen(info->rooms) = %u\n", lstlen(info->rooms));
 					break ;
 				}
 				tmp[1] = tmp[1]->next;
@@ -161,13 +157,11 @@ void		read_connections(t_lem *info, char **line)
 void		read_node(t_lem *info, char *line)
 {
 	char	**t;
-	t_lst	*tl;
 	t_room	*tr;
 
 	t = ft_strsplit(line, ' ');
 	tr = init_room(t[0], t[1], t[2]);
-	tl = lstnew(tr);
-	lstpush(&(info->rooms), tl);
+	lstpush(&(info->rooms), tr);
 	free_str_tab(&t);
 	info->num_rooms++;
 }
@@ -183,7 +177,6 @@ void		readmap(t_lem **info)
 		if (!ft_strccount(line, '#') && ft_strccount(line, '-') == 1)
 		{
 			read_connections((*info), ft_strsplit(line, '-'));
-			ft_printf("readmap[%d].conn.lstlen((*info)->rooms) = %d\n", i[0], (i[3] = lstlen((*info)->rooms)));
 			free(line);
 			continue ;
 		}
@@ -198,7 +191,6 @@ void		readmap(t_lem **info)
 		i[0]++;
 		free(line);
 	}
-	ft_printf("readmap.end.lstlen((*info)->rooms) = %d\n", lstlen((*info)->rooms));
 }
 
 void		ft_debug()
@@ -216,14 +208,9 @@ void		print_lem(t_lem *info)
 	ft_printf("info->rooms = %p\n", info->rooms);
 	ft_printf("info->start = %p\n", info->start);
 	ft_printf("info->end = %p\n", info->end);
-	// if (!info->rooms)
-	// 	return ;
-	// ft_printf("info->rooms->r = %p\n", info->rooms->r);
-	// if (!info->rooms->r)
-	// 	return ;
-	// ft_printf("info->rooms->r->name = %s\n", info->rooms->r->name);
-	// ft_printf("info->rooms->r->coord_x = %d\n", info->rooms->r->coord_x);
-	// ft_printf("info->rooms->r->coord_y = %d\n", info->rooms->r->coord_y);
+	if (!info->rooms)
+		return ;
+	ft_printf("lstlen(info->rooms) = %d\n", lstlen(info->rooms));
 }
 
 unsigned	lstlen(t_lst *l)
@@ -259,10 +246,8 @@ int			main(void)
 
 	init_lem(&info);
 	readmap(&info);
-	ft_printf("main.lstlen(info->rooms) = %d\n", lstlen(info->rooms));//Where in the world is info->rooms
 	info->end->start_end = (char)420;
 	info->start->start_end = 69;
-	ft_printf("main.lstlen(info->rooms) = %d\n", lstlen(info->rooms));
 	ft_debug();
 	print_lem(info);
 	free_lst(info->rooms);
